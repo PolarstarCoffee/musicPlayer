76c1739d2ce87e157962ddd4124024742f613157
Zuriel Zavala <zzavala2@hotmail.com>
2025-07-14 20:13:41 -0500

    vs proj

---
+++ audioPlayer.cpp
@@ -0,0 +1,28 @@
+﻿// audioPlayer.cpp : Defines the entry point for the application.
+//
+
+#include "audioPlayer.h"
+#include <iostream>
+
+using namespace std;
+
+//TODO
+//Music library (generalized directory)
+//Start
+//End
+//Fast Forward
+//Pause
+//Volume
+//GUI (customizable? Color API?)
+//Playlists
+//Queue
+
+
+
+
+//First open a directory and have it be displayed with a GUI
+int main()
+{
+	cout << "Audio player application entry point" << endl;
+	return 0;
+}
---
+++ audioPlayer.h
@@ -0,0 +1,8 @@
+﻿// audioPlayer.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+// TODO: Reference additional headers your program requires here.
